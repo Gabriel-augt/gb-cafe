530a2d8da8ba42eb3605ee35d01ef1e9dc95412e
Gabriel Augusto <117189018+Gabriel-augt@users.noreply.github.com>
2024-09-30 16:26:09 -0400

    responsive hero

--- src/components/hero/ContentHero.vue
+++ src/components/hero/ContentHero.vue
@@ -1,21 +1,25 @@
 <template>
 
-    <div class="absolute z-10 flex flex-col justify-center
-    w-max left-48 h-5/6 top-28 text-white-25">
+    <div class="absolute z-10 flex flex-col justify-center w-max left-48 h-5/6 top-28
+     text-white-25 max-[1660px]:left-28 max-[1575px]:left-8 max-[565px]:left-4
+     max-[565px]:w-96 max-[565px]:top-20">
 
-        <p class="font-light tracking-wider font-poppins">
+        <p class="font-light tracking-wider font-poppins max-[468px]:text-sm">
 
             Melhore seu humor com uma xícara de
 
         </p>
 
-        <h1 class="font-clickerScript text-[364px] -mb-11 -mt-16">
+        <h1 class="font-clickerScript text-[364px] -mb-11 -mt-16 max-[468px]:text-[228px]
+        max-[650px]:-my-8 max-[650px]:text-[300px] max-[565px]:text-[200px]
+        max-[395px]:text-[195px]">
 
             Café
 
         </h1>
 
-        <p class="font-light tracking-wider font-poppins">
+        <p class="font-light tracking-wider font-poppins
+        max-[468px]:w-96 max-[565px]:text-sm max-[395px]:w-[350px]">
 
             Descubra os melhores sabores de café que você já tomou. <br>
             Nós fornecemos o melhor para nossos clientes.
@@ -24,7 +28,8 @@
 
         <button class="relative w-48 h-16 text-xl font-bold text-black
         transition-all duration-300 ease-in-out transform shadow-lg top-8
-        bg-yellow-350 rounded-3xl font-poppins hover:scale-105">
+        bg-yellow-350 rounded-3xl font-poppins hover:scale-105
+        max-[650px]:w-40 max-[650px]:h-14">
 
             Pedir Agora
 
--- src/components/hero/Herobackground.vue
+++ src/components/hero/Herobackground.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="h-screen bg-right bg-no-repeat bg-cover w-full bg-hero">
+    <div class="h-screen bg-right bg-no-repeat bg-cover w-full bg-hero max-[780px]:bg-center">
 
         <div class="absolute w-full h-screen bg-gradient-to-r
         from-gray-950/40 to-transparent">
